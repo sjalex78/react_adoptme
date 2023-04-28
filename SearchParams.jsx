e35abee1e8aa76bcde91309a78648edccacb1405
Sarah Alexander <sjalex78@gmail.com>
2023-04-28 16:42:42 +1000

    Adding dynamic calls  in the form

--- SearchParams.jsx
+++ SearchParams.jsx
@@ -1,19 +1,54 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const BREEDS = ["Poodle"]; // access an API later
+
   return (
     <div className="search-params">
       <form>
-        <label htmlFor="location">
-          Location
-          <input
-            onChange={(e) => setLocation(e.target.value)}
-            id="location"
-            value={location}
-            placeholder="Location"
-          />
-        </label>
+        <label htmlFor="location">Location</label>
+        <input
+          onChange={(e) => setLocation(e.target.value)}
+          id="location"
+          value={location}
+          placeholder="Location"
+        />
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => {
+            updateAnimal(e.target.value);
+            updateBreed("");
+          }}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option key={animal} value={animal}>
+              {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          disabled={BREEDS.length === 0}
+          value={breed}
+          onChange={(e) => {
+            updateBreed(e.target.value);
+          }}
+        >
+          <option />
+          {BREEDS.map((breed) => (
+            <option key={breed} value={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
         <button>Submit</button>
       </form>
     </div>
